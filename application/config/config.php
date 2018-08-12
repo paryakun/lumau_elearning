e26cbe7931bb92c62de78ea3af307a9ae71a0401
paryakun <paryakun@gmail.com>
2018-08-12 19:02:51 +0700

    config changed

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost:89/education/';
-$config['base_url'] = 'http://localhost/education/';
+$config['base_url'] = 'http://localhost/lumau_elearning/';
 $config['theme_admin'] = "theme/admin";
 $config['theme_frontend'] = "theme/frontend";
 
